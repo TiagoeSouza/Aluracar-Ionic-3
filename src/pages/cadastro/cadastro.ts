28f8e9c3bb89cf3e92ae85e7caf39aa58b0524b1
unknown <tiago_e_souza@hotmail.com>
2018-09-25 20:21:10 -0300

    o codigo estava quebrando, no cadastro.ts

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -5,7 +5,6 @@ import { AgendamentosServiceProvider } from './../../providers/agendamentos-serv
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Alert, AlertController } from 'ionic-angular';
 import { Carro } from '../../modelos/carro';
-import { Agendamento } from '../../modelos/agendamento';
 
 
 @IonicPage()
